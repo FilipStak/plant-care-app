baddac151cd10b265491a4636aaf49dda25b6514
FilipStak <filip.stakor@gmail.com>
2026-03-04 20:31:15 +0100

    Initial project structure with storage and species modules

---
+++ js/species.js
@@ -0,0 +1,6 @@
+const species = {
+  calathea: { wateringFrequency: 3 },
+  cactus: { wateringFrequency: 14 },
+};
+const plantSpecies = "calathea";
+// TEST: console.log(species[plantSpecies]);
---
+++ js/storage.js
@@ -0,0 +1,11 @@
+function save(plantArray) {
+  const plantString = JSON.stringify(plantArray); //JSON.stringify() → converts object to real usable string
+  localStorage.setItem("plants", plantString); //localStorage.setItem(key, value) → stores it
+}
+//localStorage can only store strings.
+function load() {
+  const plantString = localStorage.getItem("plants"); //getItem() → retrieves string
+  if (!plantString) return [];
+  const plantArray = JSON.parse(plantString); //JSON.parse() → converts string back to object
+  return plantArray;
+}
